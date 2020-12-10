f25c968a49752f6ae27b804140c3ed94856f2ac3
Laura Osorio <laurisosorio7@hotmail.com>
2020-12-10 16:51:35 -0500

    Inicializando repositorio

---
+++ Models/ApiContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Api.Models
+{
+    public class ApiContext: DbContext
+
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
